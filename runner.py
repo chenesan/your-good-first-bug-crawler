279550522bfbc540713ce921ce519eeb88e08512
陳乙山 <pipio1994@gmail.com>
2017-01-30 16:24:39 +0800

    Update start/stop cmd

--- runner.py
+++ runner.py
@@ -1,12 +1,13 @@
+#! /usr/bin/env python
 import datetime
 import os
 import subprocess
-
 
 LOG_PATH = '/var/log/ygfb/ygfb.log'
 ERR_LOG_PATH = '/var/log/ygfb/ygfb_err.log'
 RUNNER_LOG_PATH = '/var/log/ygfb/runner.log'
 PID_PATH = '/var/log/ygfb/runner.pid'
+ENV_SH_PATH = './env.sh'
 
 pid_log = open(PID_PATH, 'w')
 pid = str(os.getpid())
@@ -30,7 +31,7 @@ while ret != -1:
         )
     )
     runner_log.flush()
-    ret = subprocess.call("node crawler.js", shell=True, stdout=log, stderr=err_log)
+    ret = subprocess.call("source {env_file}; node crawler.js".format(env_file=ENV_SH_PATH), shell=True, stdout=log, stderr=err_log)
     runner_log.write(
         '[{time}]Successfully finish {count}th crawling\n'.format(
             count=call_counter, time=datetime.datetime.now()
